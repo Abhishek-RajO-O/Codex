2a4af3c7ce545cf3b8fc37346c068a3496bff4cd
ABHISHEK RAJ <abhishekraj605912@gmail.com>
2025-07-05 01:15:49 +0530

    Easy(04/07/2025)

---
+++ LevelUp⬆️ DSA/week 1️⃣ july 2025/Easy🟢/Q04E1.java
@@ -0,0 +1,22 @@
+/*
+Date- 04/07/2025
+Q136. Single Number
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example 1:
+Input: nums = [2,2,1]
+Output: 1
+
+ */
+
+public class Q04E1 {
+    public int singleNumber(int[] nums) {
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+
+    }
+}
