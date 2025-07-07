73af690209cf4cfff0b240fadb6328404cff41c2
ABHISHEK RAJ <abhishekraj605912@gmail.com>
2025-07-07 22:45:12 +0530

    Easy(07/07/2025)

---
+++ LevelUp⬆️ DSA/week 1️⃣ july 2025/Easy🟢/Q07E1.java
@@ -0,0 +1,23 @@
+/*
+Date:- 07/07/2025
+Q191. Number of 1 Bits
+Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
+Example 1:
+Input: n = 11
+Output: 3
+Explanation:
+The input binary string 1011 has a total of three set bits.
+ */
+public class Q07E1 {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+
+}
